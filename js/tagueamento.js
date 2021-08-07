56794ca9434884e3d391df7b029994058896e2ca
Igor Arantes <igor.l.arantes@gmail.com>
2021-08-07 07:30:06 -0300

    testes e revisao

--- js/tagueamento.js
+++ js/tagueamento.js
@@ -1,10 +1,3 @@
-// Preencha este arquivo com qualquer código que você necessite para realizar a
-// coleta, desde a biblioteca analytics.js, gtag.js ou o snippet do Google Tag 
-// Manager. No último caso, não é necessário implementar a tag <noscript>.
-// O ambiente dispõe da jQuery 3.5.1, então caso deseje, poderá utilizá-la
-// para fazer a sua coleta.
-// Caso tenha alguma dúvida sobre o case, não hesite em entrar em contato.
-
 
 
 //---------Google analytics--------------//
@@ -82,6 +75,7 @@ function load(){
   
 }
 
+//Para os cards-montadoras 
 $(document).ready(function(){
     $('.cards-montadoras').on("click", '.card-montadoras',function(){
         var dataId = $(this).attr("data-id");
